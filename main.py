3d0729805cb3e35ec317d7c26354ebadb80e57ce
Bakh <imoddinov@gmail.com>
2023-05-20 15:02:33 +0500

    гг

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://langal.ru/"
+й
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.603 Yowser/2.5 Safari/537.36"
+}
+req = requests.get(url, headers=headers)
+src = req.text
+# print(src)
+
+with open("index.html", "w") as file:
+    file.write(src)
