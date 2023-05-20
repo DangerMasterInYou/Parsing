a90086bb7d71e96d3b0305864fa7416f785acd27
Bakh <imoddinov@gmail.com>
2023-05-20 16:05:05 +0500

    гг2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://langal.ru/"
-й
+йфвв
 headers = {
     "accept": "*/*",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.603 Yowser/2.5 Safari/537.36"
